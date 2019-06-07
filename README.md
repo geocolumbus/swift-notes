fb95c5dd0f97926bbe97a1dd66477bbfd9f8006b
Campbell <campbelg@campbelg-3mbpdu.oa.oclc.org>
2019-06-07 09:36:45 -0400

    Initialize swift notes repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# swift-notes
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import swift_notesTests
+
+var tests = [XCTestCaseEntry]()
+tests += swift_notesTests.allTests()
+XCTMain(tests)
