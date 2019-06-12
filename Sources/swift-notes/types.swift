c10d586a7932502ec5ab1e8addcabb642eec7a1c
George Campbell <geocolumbus@gmail.com>
2019-06-11 21:33:30 -0400

    Working it

---
+++ Sources/swift-notes/types.swift
@@ -0,0 +1,27 @@
+//
+// Created by George Campbell on 2019-06-08.
+//
+
+import Foundation
+
+// constant
+let c = 3
+print(c)
+
+// variable
+var n = 2
+n = 1
+print(n, c)
+
+// explicit type specification
+let d: Double = 13.0
+print(d)
+
+// type conversion
+var l = 33
+var s = "George"
+var t = s + String(33)
+print(t)
+
+// templating
+var u = "George used to be \(l) years old and will be \(l + l)."
